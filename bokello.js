78b8bbe4b4464a4ec148a5fd8b7f73e99bef51e5
Heikki Toivanen <heikki.t.toivanen@aalto.fi>
2019-03-21 20:52:49 +0200

    First version

---
+++ bokello.js
@@ -0,0 +1,105 @@
+var taulu, aika, nimi, saveArea;
+var timerIDs = [];
+var secsGone = 0;
+
+function init() {
+  taulu = document.getElementById('taulu');
+  aika = document.getElementById('aika');
+  nimi = document.getElementById('nimi');
+  saveArea = document.getElementById('saveArea');
+
+  for (var i = 0; i < localStorage.length; i++) {
+    var data = JSON.parse(localStorage.getItem(i));
+    var name = data.shift();
+    var bid = data.shift();
+    var button = document.createElement("button");
+    button.id = bid;
+    button.textContent = name;
+    button.addEventListener ("click", function() {
+      loadData(this.id);
+    });
+    saveArea.appendChild(button);
+  }
+}
+
+function addRow(s, m, t) {
+  var newRow = taulu.insertRow(-1);
+  var c1 = newRow.insertCell(0);
+  var c2 = newRow.insertCell(1);
+  var c3 = newRow.insertCell(2);
+
+  c1.appendChild(document.createTextNode(s));
+  c2.appendChild(document.createTextNode(m));
+  c3.appendChild(document.createTextNode(t));
+  c1.contentEditable = true;
+  c2.contentEditable = true;
+  c3.contentEditable = true;
+}
+
+function saveData() {
+  var d = [nimi.textContent, localStorage.length];
+  for (var i = 1; i < taulu.rows.length; i++) {
+    var secs = taulu.rows[i].cells[0].textContent;
+    var mins = taulu.rows[i].cells[1].textContent;
+    var text = taulu.rows[i].cells[2].textContent;
+    d.push(secs,mins,text);
+  }
+  var data = JSON.stringify(d);
+  console.log(data);
+  localStorage.setItem(localStorage.length, data);
+
+  var button = document.createElement("button");
+  button.id = d[1];
+  button.textContent = d[0];
+  button.addEventListener ("click", function() {
+    loadData(this.id);
+  });
+  saveArea.appendChild(button);
+}
+
+function loadData(sid) {
+  var d = localStorage.getItem(sid);
+  console.log(d);
+  var data = JSON.parse(d);
+  nimi.textContent = data.shift();
+  data.shift(); //ID not needed
+  while (taulu.rows.length > 1) {
+    taulu.deleteRow(1);
+  }
+  while (data.length > 0) {
+    var secs = data.shift();
+    var mins = data.shift();
+    var text = data.shift();
+    addRow(secs, mins, text);
+  }
+}
+
+function start() {
+  stop()
+  for (var i = 1; i < taulu.rows.length; i++) {
+    var secs = parseInt(taulu.rows[i].cells[0].textContent) * 60 + parseInt(taulu.rows[i].cells[1].textContent);
+    var tid = window.setTimeout(say, secs*1000, taulu.rows[i].cells[2].textContent);
+    timerIDs.push(tid);
+  }
+  var tid = window.setInterval(updateTime, 1000);
+  timerIDs.push(tid);
+}
+
+function updateTime() {
+  secsGone += 1;
+  aika.textContent = Math.floor(secsGone / 60) + ":" + (secsGone % 60).toString().padStart(2, '0');
+}
+
+function stop() {
+  while (timerIDs.length > 0) {
+    var tid = timerIDs.pop();
+    clearTimeout(tid);
+  }
+  secsGone = -1;
+  updateTime();
+}
+
+function say(text) {
+  var msg = new SpeechSynthesisUtterance(text);
+  window.speechSynthesis.speak(msg);
+}
